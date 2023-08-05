bea8a77a64a57beafd0692af3ca1539b5f49a2c5
Martin Storgaard Dieu <Mastdieu@protonmail.com>
2023-08-05 22:28:46 +0200

    Add PSKCONTEXT_EXPECTED_OPENSSL_VERSION environment variable to make tests flexible

--- tests/test_openssl_version_info.py
+++ tests/test_openssl_version_info.py
@@ -11,14 +11,22 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from os import getenv
 from ssl import OPENSSL_VERSION_INFO
 
 from pskcontext import PSKContext
+
+
+def _expected_openssl_version_info():
+    expected_openssl_version = getenv("PSKCONTEXT_EXPECTED_OPENSSL_VERSION", None)
+    if expected_openssl_version is None:
+        return OPENSSL_VERSION_INFO
+    assert "3.0.8", "Currently only OpenSSL 3.0.8 or built-in is supported"
+    return 3, 0, 0, 8, 0
 
 
 def test_openssl_version_info():
     context = PSKContext()
 
     version_info = context.openssl_version_info
-
-    assert OPENSSL_VERSION_INFO == version_info
+    assert _expected_openssl_version_info() == version_info
