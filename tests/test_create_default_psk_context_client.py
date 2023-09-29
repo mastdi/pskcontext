4fbf338bffa28c2a122f46f3ea3253a011958b12
Martin Storgaard Dieu <Mastdieu@protonmail.com>
2023-09-29 23:26:58 +0200

    Remove test for typing guard

--- tests/test_create_default_psk_context_client.py
+++ tests/test_create_default_psk_context_client.py
@@ -1,7 +1,5 @@
 import ssl
 
-import pytest
-
 from pskcontext import Purpose, create_default_psk_context
 
 
@@ -9,12 +7,6 @@ def test_create_default_psk_context():
     context = create_default_psk_context(Purpose.CLIENT_AUTH, psk=b"")
     assert issubclass(type(context), ssl.SSLContext)
     assert context.protocol == ssl.PROTOCOL_TLS_CLIENT
-
-
-def test_create_default_psk_context_unsupported_purpose():
-    with pytest.raises(TypeError):
-        # noinspection PyTypeChecker
-        create_default_psk_context("123", psk=b"123")
 
 
 def test_ssl_object():
